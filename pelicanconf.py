01f41f7cda1624e5473354b7e8fd7f53767d0842
heispeg <314308943@qq.com>
2023-06-22 22:43:36 +0800

    changed some things

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,14 +16,12 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('osu!', 'https://osu.ppy.sh/'),
+         ('MCBBS','https://www.mcbbs.net'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/heispeg'),
+          ('Pixiv', 'https://www.pixiv.net/users/23193018'),)
 
 DEFAULT_PAGINATION = 10
 
