4ecf039ab679413b8c61ced0c723977f0e49b57f
gepsieh <314308943@qq.com>
2026-05-12 21:54:36 +0800

    modify formula format

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,17 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+  },
+  options: {
+    ignoreHtmlClass: '.*',
+    processHtmlClass: 'arithmatex',
+  },
+};
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
