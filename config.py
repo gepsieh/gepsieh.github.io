33d01f25aa7678c304c46521d99db1f289ad6855
gepsieh <314308943@qq.com>
2026-05-13 20:03:03 +0800

    remove useless stuff

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_PATH = os.path.join(BASE_DIR, 'mkdocs.yml')
-UPLOAD_DIR = os.path.join(BASE_DIR, 'docs')
-IMAGE_DIR = os.path.join(UPLOAD_DIR, 'images')
-DRAFTS_DIR = os.path.join(BASE_DIR, 'drafts')
-IGNORED_DIRS = {'images', 'javascripts'}
-INDEX_LIMIT = 10
